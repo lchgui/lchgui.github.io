43e49f3e91d37728dc63e73ab45f5c4908ab119d
lchg120 <282021406@qq.com>
2026-05-09 08:10:36 +0800

    clear

--- static/js/use-deprecated-CUCv6fod.js
+++
@@ -1,3 +0,0 @@
-import{Nt as e,Yr as t,di as n}from"./_plugin-vue_export-helper-CFEgyT5i.js";var r=({from:r,replacement:i,scope:a,version:o,ref:s,type:c=`API`},l)=>{t(()=>n(l),t=>{t&&e(a,`[${c}] ${r} is about to be deprecated in version ${o}, please use ${i} instead.
-For more detail, please visit: ${s}
-`)},{immediate:!0})};export{r as t};
